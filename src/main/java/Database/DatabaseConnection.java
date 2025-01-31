c7c5f773e79db9395c128bcf0a0cea79e9596c3c
SenalAbeysekara <senaldushyantha@gmail.com>
2025-01-31 23:30:49 +0530

    Manage Pharmacy Inventory

--- src/main/java/Database/DatabaseConnection.java
+++ src/main/java/Database/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     private static final String DB_USER = "root"; // Replace with your username
     private static final String DB_PASSWORD = ""; // Replace with your password
 
-    // Method to get a connection
+    // Method to get connection
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
