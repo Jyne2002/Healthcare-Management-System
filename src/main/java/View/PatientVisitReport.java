8f6fa1a8a20d79f1975eabbbd8435485b5bc3f84
ThejanMihisara <thejanmihisara077@gmail.com>
2025-02-02 03:50:50 +0530

    manage doctor schedule

--- src/main/java/View/PatientVisitReport.java
+++ src/main/java/View/PatientVisitReport.java
@@ -2,7 +2,7 @@ package View;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
