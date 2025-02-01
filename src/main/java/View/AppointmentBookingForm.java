bb876e10bd4df0e1ed099f7c3dc6054747bf1441
HESHON2004 <heshaneshel321@gmail.com>
2025-02-01 23:43:21 +0530

    appoinment

--- src/main/java/View/AppointmentBookingForm.java
+++ src/main/java/View/AppointmentBookingForm.java
@@ -31,7 +31,7 @@ public class AppointmentBookingForm extends JFrame {
         JPanel containerPanel = new JPanel(new GridBagLayout());
         containerPanel.setBackground(new Color(240, 240, 240));  // Light gray background for the window
         containerPanel.add(formPanel);
-
+//appointment
         // Set JFrame properties
         add(containerPanel, BorderLayout.CENTER);
         setSize(600, 600);  // Increase size to make the form bigger
